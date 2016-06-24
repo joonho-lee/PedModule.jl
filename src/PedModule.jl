0ef17ae3c541a4fa9ab7ffc51acd8ce1a71b891f
reworkhow <reworkhow@gmail.com>
2016-06-24 15:49:50 -0500

    speed up calculate Ai

--- src/PedModule.jl
+++ src/PedModule.jl
@@ -106,45 +106,9 @@ function calcInbreeding!(ped::Pedigree,id::AbstractString)
 end
 
 function AInverse(ped::Pedigree)
-    n = ped.currentID - 1
-    Ai = spzeros(n,n)
-    pos  = Int64[0,0,0]
-    q    = [0.5,0.5,1.0]
-    for ind in keys(ped.idMap)
-        sire = ped.idMap[ind].sire
-        dam  = ped.idMap[ind].dam
-        pos[1] = sire=="0" ? 0: ped.idMap[sire].seqID
-        pos[2] = dam =="0" ? 0: ped.idMap[dam ].seqID
-        pos[3] = ped.idMap[ind].seqID
-        if pos[1]>0 && pos[2]>0
-            q[1] = -0.5
-            q[2] = -0.5
-            d = 4.0/(2 - ped.idMap[sire].f - ped.idMap[dam].f)
-        elseif pos[1]>0
-            q[1] = -0.5
-            q[2] = 0.0
-            d = 4.0/(3 - ped.idMap[sire].f)
-        elseif pos[2]>0
-            q[1] = 0.0
-            q[2] = -0.5
-            d = 4.0/(3 - ped.idMap[dam].f)
-        else
-            q[1] = 0.0
-            q[2] = 0.0
-            d = 1.0
-        end
-        for i=1:3
-            ii = pos[i]
-            if ii>0
-                for j=1:3
-                    jj = pos[j]
-                    if jj>0
-                        Ai[ii,jj] += q[i]*q[j]*d
-                    end
-                end
-            end
-        end
-    end
+    ii,jj,vv = HAi(ped)#cholesky??
+    hAi      = sparse(ii,jj,vv)
+    Ai       = hAi'hAi
     return Ai
 end
 
@@ -230,5 +194,6 @@ function getIDs(ped::Pedigree)
 end
 
 include("forSSBR.jl")
+include("AInverseSlow.jl")
 
 end # of PedModule
